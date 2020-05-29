f2a0cc30b580fa80e44282c896a15960941ef290
janvi-vp <janvi.vp@somaiya.edu>
2020-05-29 19:33:54 +0530

    insert, update, delete routes for patient done

--- src/controllers/patientController.js
+++ src/controllers/patientController.js
@@ -17,4 +17,86 @@ const testReq = async (req, res) => {
   }
 };
 
-export { testReq };
+const patientInsertReq = async (req, res) => {
+  try {
+    const {
+      _id,
+      name,
+      dob,
+      phone,
+      gender,
+      email,
+      diseases,
+      appointments,
+    } = req.body;
+    const NewPatient = new Patient({
+      _id: _id,
+      name: name,
+      dob: dob,
+      phone: phone,
+      gender: gender,
+      email: email,
+      diseases: diseases,
+      appointments: appointments,
+    });
+
+    NewPatient.save((err, patient) => {
+      if (err) {
+        return res.send({
+          type: "error",
+          heading: "Server Error",
+          _error: "Please try again later",
+        });
+      } else {
+        console.log(patient);
+        return res.send(patient);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Error");
+  }
+};
+
+const updatePatientReq = async (req, res) => {
+  try {
+    const { _id, appointments } = req.body;
+
+    const UpdatePatient = await Patient.updateOne(
+      { _id: _id },
+      { $set: { appointments: appointments } }
+    );
+
+    if (!UpdatePatient)
+      return res.send({
+        type: "error",
+        heading: "Error",
+        _error: "Could not be updated",
+      });
+
+    res.send(UpdatePatient);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Error");
+  }
+};
+
+const deletePatientReq = async (req, res) => {
+  try {
+    const _id = req.query._id;
+    const DeletePatient = await Patient.deleteOne({ _id: _id });
+    if (!DeletePatient)
+      return res.send({
+        type: "error",
+        heading: "Error",
+        _error: "Could not be updated",
+      });
+
+    res.send(DeletePatient);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Internal Error");
+  }
+};
+
+export { testReq, patientInsertReq, updatePatientReq, deletePatientReq };
--- src/routers/patientRouter.js
+++ src/routers/patientRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 
-import { testReq } from "../controllers/patientController";
+import {
+  testReq,
+  patientInsertReq,
+  updatePatientReq,
+  deletePatientReq,
+} from "../controllers/patientController";
 
 const router = Router();
 
 router.route("/test").post(testReq);
+router.route("/insert").post(patientInsertReq);
+router.route("/update").put(updatePatientReq);
+router.route("/delete").delete(deletePatientReq);
 
 export default router;
